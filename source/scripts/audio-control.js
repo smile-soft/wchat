6f4abf51b503e1ea7c795e10e3a85012f3f9c453
Oleksiy Brain <arro.social@gmail.com>
2017-01-13 00:09:23 +0200

    Add sound

---
+++ source/scripts/audio-control.js
@@ -0,0 +1,27 @@
+var audioEl;
+var dir = '';
+var format = 'wav';
+var volume = 0.5;
+
+module.exports = {
+
+	init: function(soundsDir) {
+		audioEl = document.createElement('audio');
+		audioEl.setAttribute('autoplay', true);
+		if(soundsDir) dir = soundsDir;
+		return this;
+	},
+
+	play: function(filename, loop) {
+		if(filename) audioEl.src = (dir + filename + '.' + format);
+		loop ? audioEl.setAttribute('loop', true) : audioEl.removeAttribute('loop');
+		audioEl.volume = volume;
+		audioEl.play();
+	},
+
+	stop: function() {
+		audioEl.pause();
+		audioEl.currentTime = 0;
+	}
+
+};
